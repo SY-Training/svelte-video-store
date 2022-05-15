50e0069377e477e2ab62be5e8452c497f884103d
S-Y <sy-unimportant@outlook.com>
2022-05-15 12:56:10 +0100

    changes to app file

--- video-project/src/App.svelte
+++ video-project/src/App.svelte
@@ -1,30 +1,39 @@
 <script lang="ts">
-	export let name: string;
+  export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <header>
+    <!-- header info -->
+  </header>
+  <mid>
+    <p>This will be videos.</p>
+    <!-- vid list -->
+  </mid>
+  <footer>
+    <!-- disclaimer -->
+  </footer>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    display: flex;
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
